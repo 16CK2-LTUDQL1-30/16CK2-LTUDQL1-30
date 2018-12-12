29cefe011db33b460c33381410d4845c109c23b1
TranTrongNghia1660370 <44776050+TranTrongNghia1660370@users.noreply.github.com>
2018-12-12 16:53:40 +0700

    GUI
    GUI

---
+++ Do_An/GUI/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public partial class frmQuanLyVeXe : Form
+    {
+        public frmQuanLyVeXe()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmQuanLyVeXe_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (MessageBox.Show("Bạn có muốn thoát khỏi ứng dụng không?", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                Dispose();//thoát chương trình
+            }
+        }
+    }
+}
